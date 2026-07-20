af996775b80e8aed2c773b88eac75b76c45521f7
Riddhi Pachehara <riddhipachehara@gmail.com>
2026-07-20 16:04:33 +0530

    Display teachers list from database

--- school/views.py
+++ school/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Teacher
 
 def home(request):
     return render(request, 'school/home.html')
 
 def about(request):
     return render(request, 'school/about.html')
+
+def teachers(request):
+    teachers = Teacher.objects.all()
+    return render(request, 'school/teachers.html', {
+        'teachers': teachers
+    })
