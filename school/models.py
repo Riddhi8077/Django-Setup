3f625f56d377b3572eb8c3b5702e9071b7c5f4df
Riddhi Pachehara <riddhipachehara@gmail.com>
2026-07-20 14:43:38 +0530

    Add Classroom model with School relationship

--- school/models.py
+++ school/models.py
@@ -9,3 +9,12 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Classroom(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    class_name = models.CharField(max_length=50)
+    room_number = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.class_name
