ccdab43c86b15b70d8ec7b84bd3b5f4da2d68ba1
Riddhi Pachehara <riddhipachehara@gmail.com>
2026-07-20 14:36:40 +0530

    Add School model and configure Django admin

--- school/models.py
+++ school/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    principal = models.CharField(max_length=100)
+    established_year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
